50bf1f35a47ef7e305974fe19ae0c33ee6a9ae0d
khanhtuan623 <khanhtuan623@gmail.com>
2017-12-06 16:23:24 +0700

    update project-2

--- Project-2/src/sv/practice/mysql/JDBCStatement.java
+++ Project-2/src/sv/practice/mysql/JDBCStatement.java
@@ -3,25 +3,32 @@ package sv.practice.mysql;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import practice.sv.bai1.Student;
 
 public class JDBCStatement {
-		public static void statement() {
+	public static List<Student> readData(){
 		try {
-		Statement statement=JDBCConectionMysql.getJDBCConnection().createStatement();
-		String sql="SELECT * FROM student.info";
-		ResultSet rs=statement.executeQuery(sql);
-		while(rs.next())
-		{
-			int id=rs.getInt("id");
-			String fname=rs.getString("fname");
-			String lname=rs.getString("lname");
-			String bd=rs.getString("bd");
-			String email=rs.getString("email");
-			System.out.println(id+" "+fname+" "+lname+" "+bd+" "+email);
-		}
-		} catch (SQLException e) {
+			ArrayList<Student>listS=new ArrayList<Student>();
+			Statement statement=JDBCConectionMysql.getJDBCConnection().createStatement();
+			String sql="SELECT * FROM internship.student";
+			ResultSet rs=statement.executeQuery(sql);
+			while(rs.next())
+			{
+				int id=rs.getInt("id");
+				String lname=rs.getString("last_name");
+				String fname=rs.getString("first_name");
+				String bd=rs.getString("birth_day");
+				String email=rs.getString("email");
+				listS.add(new Student(lname, fname, bd, email));
+			}
+				return listS;
+			} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return null;
 	}
 }
